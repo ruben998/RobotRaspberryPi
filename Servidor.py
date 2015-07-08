488a47f9b04ccb97930388e12d9256e466ce1f9e
ruben998 <rnietova7@alumnes.ub.edu>
2015-07-08 22:26:02 +0200

    Bugs: Close socket and move of the robot

--- Servidor.py
+++ Servidor.py
@@ -1,43 +1,29 @@
 #!/usr/bin/env python
  
-#importamos el modulo socket
 import socket
 import RPi.GPIO as GPIO
 import time
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
+import thread
 
 def adelante():
+    stop()
     GPIO.output(11, True)
     GPIO.output(13, True)
-    time.sleep(0.2)
-    GPIO.output(11, False)
-    GPIO.output(13, False)
 
 def atras():
+    stop()
     GPIO.output(15, True)
     GPIO.output(7, True)
-    time.sleep(0.2) 
-    GPIO.output(15, False)
-    GPIO.output(7, False)
 
 def derecha():
+    stop()
     GPIO.output(7, True)
     GPIO.output(13, True)
-    time.sleep(0.2) 
-    GPIO.output(7, False)
-    GPIO.output(13, False)
 
 def izquierda():
+    stop()
     GPIO.output(11, True)
     GPIO.output(15, True)
-    time.sleep(0.2)     
-    GPIO.output(11, False)
-    GPIO.output(15, False)
 
 def stop():
     GPIO.output(7, False)
@@ -65,41 +51,31 @@ def menu(opcio):
         stop()
         return 0
 
+def handler(clientsocket, clientaddr):
+    while True:
+	data = clientsocket.recv(1024)
+	if not data:
+	    break
+	else:
+	    menu(data)
+    clientsocket.close()
+
 def main():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.OUT)
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.setup(13, GPIO.OUT)
+    GPIO.setup(15, GPIO.OUT)
+
     stop() 
-    #instanciamos un objeto para trabajar con el socket
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
      
-    #Con el metodo bind le indicamos que puerto debe escuchar y de que servidor esperar conexiones
-    #Es mejor dejarlo en blanco para recibir conexiones externas si es nuestro caso
-    print 'Esperando cliente'
     s.bind(("", 9999))
      
-    #Aceptamos conexiones entrantes con el metodo listen, y ademas aplicamos como parametro
-    #El numero de conexiones entrantes que vamos a aceptar
     s.listen(1)
-     
-    #Instanciamos un objeto sc (socket cliente) para recibir datos, al recibir datos este 
-    #devolvera tambien un objeto que representa una tupla con los datos de conexion: IP y puerto
-    sc, addr = s.accept()
-     
-     
     while True:
-        #if(sc.timeout)
-       
-        recibido = sc.recv(1024)
-     
-       	if menu(recibido) == -1:
-            sc.close()
-       
-        print str(addr[0]) + " dice: ", recibido
-     
-        #Devolvemos el mensaje al cliente
-        sc.send(recibido)
-    print "Adios."
-     
-    #Cerramos la instancia del socket cliente y servidor
-    #sc.close()
-    #s.close()
-
+	sc, addr = s.accept()
+	thread.start_new_thread(handler,(sc,addr))
 main()
